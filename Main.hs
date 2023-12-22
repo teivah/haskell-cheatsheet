a8808f74ccca9f1d0a52a6d2aa9e1b45ee0e6348
Teiva Harsanyi <teiva.harsanyi@gmail.com>
2023-12-22 19:28:41 +0100

    Haskell 101

--- Main.hs
+++ Main.hs
@@ -1,2 +1,19 @@
 main :: IO ()
-main = putStrLn "Hello , xWorld a !"
+main = do
+  -- Variable
+  let a = 42
+  print a
+
+  -- Function call
+  let a = double 5
+  -- The same function works with floats
+  let a = double 5.2
+
+  -- Conditions
+  let a = doubleIfCondition 10
+
+  print ""
+
+double x = x * 2
+
+doubleIfCondition x = if x > 100 then x else x * 2
